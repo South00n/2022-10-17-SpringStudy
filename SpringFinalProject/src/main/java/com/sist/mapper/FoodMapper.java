f7d972298baa03044ac130d6cf9e65e1631bfbaf
south00n <nhk828@naver.com>
2023-03-09 17:00:13 +0900

    23.03.09 vue

--- SpringFinalProject/src/main/java/com/sist/mapper/FoodMapper.java
+++ SpringFinalProject/src/main/java/com/sist/mapper/FoodMapper.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.sist.mapper;
 
 import java.util.*;
@@ -58,37 +57,3 @@ public interface FoodMapper {
 			+ "WHERE rownum <= 7")
 	public List<FoodVO> foodTop7();
 }
-=======
-package com.sist.mapper;
-
-import java.util.*;
-
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
-
-import com.sist.vo.*;
-
-public interface FoodMapper {
-	@Select("SELECT cno, title, poster "
-			+ "FROM project_category ORDER BY cno ASC")
-	public List<CategoryVO> categoryListData();
-	
-	@Select("SELECT title, subject FROM project_category "
-			+ "WHERE cno = #{cno}")
-	public CategoryVO categoryInfoData(int cno);
-	
-	@Select("SELECT fno, name, poster, score, tel, address, type "
-			+ "FROM project_food "
-			+ "WHERE cno = #{cno}")
-	public List<FoodVO> foodListData(int cno);
-	
-	@Update("UPDATE project_food SET "
-			+ "hit = hit+1 "
-			+ "WHERE fno=#{fno}")
-	public void foodHitIncrement(int fno);
-	
-	@Select("SELECT * FROM project_food "
-			+ "WHERE fno=#{fno}")
-	public FoodVO foodDetailData(int fno);
-}
->>>>>>> branch 'master' of https://github.com/South00n/2022-10-17-SpringStudy.git
--- SpringFinalProject/src/main/java/com/sist/web/FoodFController.java
+++ SpringFinalProject/src/main/java/com/sist/web/FoodFController.java
@@ -13,38 +13,44 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.sist.vo.*;
 import com.sist.dao.*;
-
 @Controller
+// front : router(include)
+/*
+ *   vue => vue.component
+ */
 public class FoodFController {
-	@Autowired
-	private FoodDAO dao;
-	
-	@GetMapping("food/food_list.do")
-	public String food_list(int cno, Model model) {
-		model.addAttribute("cno", cno);
-		return "food/food_list";
-	}
-	@GetMapping("food/food_before_detail.do")
-	public String food_detail_before(int fno, HttpServletResponse response, RedirectAttributes ra) {
-		
-		Cookie cookie = new Cookie("spring_food"+fno, String.valueOf(fno));
-		cookie.setPath("/");
-		cookie.setMaxAge(60*60*24);
-		response.addCookie(cookie);
-		
-		ra.addAttribute("fno", fno);
-		return "redirect:../food/food_detail.do";
-	}
-	
-	@GetMapping("food/food_detail.do")
-	public String food_detail(int fno, Model model) {
-		model.addAttribute("fno", fno);
-		return "food/food_detail";
-	}
-	
-	@GetMapping("food/food_find.do")
-	public String food_find() {
-		return "food/food_find";
-	}
-	
+   @Autowired
+   private FoodDAO dao;
+   
+   @GetMapping("food/food_list.do")
+   //?cno=
+   public String food_list(int cno,Model model)
+   {
+	   model.addAttribute("cno", cno);
+	   return "food/food_list";
+   }
+   
+   @GetMapping("food/food_before_detail.do")
+   public String food_before_detail(int fno,HttpServletResponse response,RedirectAttributes ra)
+   {
+	   Cookie cookie=new Cookie("spring_food"+fno, String.valueOf(fno));
+	   cookie.setPath("/");
+	   cookie.setMaxAge(60*60*24);
+	   response.addCookie(cookie);
+	   ra.addAttribute("fno", fno);
+	   return "redirect:../food/food_detail.do";
+   }
+   
+   @GetMapping("food/food_detail.do")
+   public String food_detail(int fno,Model model)
+   {
+	   model.addAttribute("fno", fno);
+	   return "food/food_detail";
+   }
+   
+   @GetMapping("food/food_find.do")
+   public String food_find()
+   {
+	   return "food/food_find";
+   }
 }
